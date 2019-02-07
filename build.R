9fe4136b2354152529efbecf7bdeaaf351aba14a
Max Murphy <murphy2122@berkeley.edu>
2019-02-06 22:27:03 -0800

    update index.html

--- build.R
+++ build.R
@@ -1,5 +1,5 @@
 library(rmarkdown)
 library(here)
 
-rmarkdown::render(here("R", "00_rcpp_intro.Rmd"), output_file = here("docs", "rcpp_intro.html"))
+rmarkdown::render(here("R", "00_rcpp_intro.Rmd"), output_file = here("docs", "index.html"))
 rmarkdown::render(here("R", "01_setup.Rmd"), output_file = here("docs", "setup.html"))
