1058723370fadfb19efaf4ffa98067eed41fe4c8
Max Murphy <murphy2122@berkeley.edu>
2019-02-06 22:33:40 -0800

    build site

--- build.R
+++ build.R
@@ -3,3 +3,5 @@ library(here)
 
 rmarkdown::render(here("R", "00_rcpp_intro.Rmd"), output_file = here("docs", "index.html"))
 rmarkdown::render(here("R", "01_setup.Rmd"), output_file = here("docs", "setup.html"))
+
+rmarkdown::render_site(here("R"),)
